a44e9bfb3b19a72bd1fc34fdec801139afc3f306
Nonamerz <a.g.kolesnik@yandex.ru>
2020-06-22 16:27:57 +0300

    Initial Commit

---
+++ Lesson10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson10
+//
+//  Created by MacBook Pro on 22.06.2020.
+//  Copyright © 2020 Artem K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
